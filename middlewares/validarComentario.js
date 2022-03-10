20179718f42a00a307ed4250c6d0cf0aeb315bcf
Bruno Ruiz <ruiz.brunoivan@gmail.com>
2022-03-10 13:34:22 -0200

    Se agregan funciones de validacion

---
+++ middlewares/validarComentario.js
@@ -0,0 +1,19 @@
+const {request, response} = require('express');
+const ComentarioSchema = require('../models/Comentario');
+
+const validarCoincidenciaID = (req = request, res = response, next) => {
+    const {usuario} = req;
+    const {cid} = req.body;
+    const comentario = await ComentarioSchema.findOne({_id:cid});
+    if(!comentario)
+        return res.status(401).send({msg:'El comentario no se encuentra disponible'});
+    if(usuario._id != comentario.uid)
+        return res.status(401).send({msg:'No se puede realizar dicha acción'});
+    
+    req.comentario = comentario;
+    next(); 
+};
+
+module.exports = {
+    validarCoincidenciaID
+}
---
+++ middlewares/validarPublicacion.js
@@ -0,0 +1,20 @@
+const {request, response} = require('express');
+const PublicacionSchema = require('../models/Publicacion');
+
+const validarExistenciaPublicacion = (req = request, res = response, next) => {
+    const {pid} = req.body;
+    let publicacion = await PublicacionSchema.findById({_id:pid});
+    if(!publicacion)
+        return res.status(404).send({msg:'La publicación no existe'});
+    req.publicacion = publicacion;
+    next();
+};
+
+const validarCoincidenciaID = (req = request, res = response, next) => {
+    const {usuario, publicacion} = req;
+    if(usuario._id != publicacion.uid)
+        return res.status(400).send({msg:'Error al querer borrar publicacion'});
+    next();
+};
+
+module.exports = { validarExistenciaPublicacion, validarCoincidenciaID };
