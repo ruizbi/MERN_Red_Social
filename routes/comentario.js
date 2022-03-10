b16a7c79426ed9e5dda61c8f2e45371d72b8832b
Bruno Ruiz <ruiz.brunoivan@gmail.com>
2022-03-10 13:33:05 -0200

    Se añaden middlewares para modularizar el codigo de los services

--- routes/comentario.js
+++ routes/comentario.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validarCampos');
+const { validarCoincidenciaID } = require('../middlewares/validarComentario');
 const validarJWT = require('../middlewares/validarJWT');
 const { crearComentario, borrarComentario, changeLike_Comentario, modificarComentario } = require('../services/comentario');
 const router = express.Router();
@@ -17,18 +18,21 @@ router.delete('/borrar_comentario', [
     check('pid', 'El ID de la publicacion esobligatorio'),
     validarCampos,
     validarJWT,
+    validarCoincidenciaID,
     borrarComentario]);
 
 router.put('/change_like', [
     check('cid', 'El ID del comentario es obligatorio'),
     validarCampos,
     validarJWT,
+    validarCoincidenciaID,
     changeLike_Comentario]);
 
 router.put('/modificar_comentario', [
     check('message', 'El mensaje es obligatorio'),
     validarCampos,
     validarJWT,
+    validarCoincidenciaID,
     modificarComentario]);
     
 module.exports = router;
--- routes/publicacion.js
+++ routes/publicacion.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validarCampos');
+const { validarExistenciaPublicacion, validarCoincidenciaID } = require('../middlewares/validarPublicacion');
 const validarJWT = require('../middlewares/validarJWT');
 const { crearPublicacion, borrarPublicacion, modificarPublicacion, changeLike, buscarPublicacion } = require('../services/publicacion');
 const router = express.Router();
@@ -10,13 +11,14 @@ router.post('/crear_publicacion', [
     check('descripcion', 'La descripcion es obligatoria').notEmpty(),
     validarCampos,
     validarJWT,
-    crearPublicacion
-    ]);
+    crearPublicacion]);
 
 router.delete('/borrar_publicacion', [
     check('pid', 'El ID de la publicacion es obligatorio').notEmpty(),
     validarCampos,
+    validarExistenciaPublicacion,
     validarJWT,
+    validarCoincidenciaID,
     borrarPublicacion]);
 
 router.put('/modificar_publicacion', [
@@ -24,14 +26,19 @@ router.put('/modificar_publicacion', [
     check('imagen', 'El path de la imagen es obligatorio').notEmpty(),
     check('descripcion', 'La descripcion es obligatoria').notEmpty(),
     validarCampos,
+    validarExistenciaPublicacion,
     validarJWT,
+    validarCoincidenciaID,
     modificarPublicacion]);
 
 router.put('/change_like', [
     check('pid', 'El ID de la publicacion es obligatorio').notEmpty(),
+    validarExistenciaPublicacion,
     validarJWT,
     changeLike]);
 
-router.get('/:pid', buscarPublicacion);
+router.get('/:pid', [
+    validarJWT,
+    buscarPublicacion]);
 
 module.exports = router;
